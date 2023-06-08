7309131a4b1f0bdd37c0fb5904ec66d662fd9680
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2023-06-08 20:46:44 +0300

    cats and shelves refactor

--- 7 kyu/cats-and-shelves.js
+++ 7 kyu/cats-and-shelves.js
@@ -1,12 +1,5 @@
 function solution(start, finish) {
-  if (start == finish) return 0
-  let current = start, trippleCount = 0, trippleValue = start
+  const diff = finish - start
   
-  while (current + 3 <= finish) {
-    trippleCount++
-    trippleValue += 3
-    current += 3
-  }
-  
-  return trippleCount + finish - trippleValue
+  return Math.floor(diff / 3) + diff % 3
 }
